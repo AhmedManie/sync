cf01eaf5f20d4fca2c29d1dedb0955159efcf619
Null <stephanoashraf64@gmail.com>
2026-03-13 12:28:34 +0200

    added main with font

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <SFML/Audio.hpp>
+#include <iomanip>
+using namespace std;
+using namespace sf;
+
+// GLOBAL VARIABLES
+Event event;
+RenderWindow window = { VideoMode::getDesktopMode(), "Icy Towers" };
+Vector2u windowSize = window.getSize();
+View gameCamera = window.getDefaultView();
+int menuIndex = 1;
+
+Clock timeClock;
+Clock dtClock;
+float dt = 0;
+
+// test
+// Main
+int main() {
+    while (window.isOpen()) {
+        dt = dtClock.restart().asSeconds() * 10;
+
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+			}
+
+			// Game logic goes here
+
+
+		// Drawing goes here
+
+       
+    }
+    return 0;
+}
